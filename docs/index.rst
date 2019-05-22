33784b01747ff10a2e34f49ab351bfd141484176
Travis J. Lawrence <tlawrence3@ucmerced.edu>
2019-05-22 10:05:18 -0400

    Staged files to commit this time

---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. DISCo documentation master file, created by
+   sphinx-quickstart on Wed May 22 09:44:08 2019.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to DISCo's documentation!
+=================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,45 @@
+import re
+import os
+from setuptools import setup, Extension
+from codecs import open
+from os import path
+
+version_file = open("DISCO/_version.py", "r").read()
+version_match = re.match(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file)
+if (version_match):
+    version = version_match.group(1)
+else:
+    raise RuntimeError("Unable to find version string in _version.py")
+
+here = path.abspath(path.dirname(__file__))
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+
+setup(name = "tsfm",
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
+      install_requires=['biopython'],
+      packages = ["DISCO"],
+      entry_points = {
+          "console_scripts": ['DISCO = DISCO.DISCO:main']},
+      version = version,
+      author="Dana L. Carper, Travis J. Lawrence, Alyssa A. Carrell, Dale A. Pelletier, and David J. Weston",
+      author_email="carperdl@ornl.gov",
+      description = "Design of identifiable synthetic communities",
+      long_description=long_description,
+      license='LGPLv3',
+      url = "https://github.com/dlcarper/DISCo-Design-of-an-identifiable-synthetic-community",
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Console',
+                   'Intended Audience :: Science/Research',
+                   'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+                   'Natural Language :: English',
+                   'Operating System :: MacOS :: MacOS X',
+                   'Operating System :: POSIX :: Linux',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6',
+                   'Programming Language :: Python :: 3.7',
+                   'Programming Language :: Python :: Implementation :: CPython',
+                   'Topic :: Scientific/Engineering :: Bio-Informatics'],)
